7192e4898ffbc91d98ba790bbc4e79f6906be895
Coldzer0 <6647006+Coldzer0@users.noreply.github.com>
2022-11-27 10:10:56 +0200

    Support IDA 7.7 & Fix some bug

--- Delphi.py
+++ Delphi.py
@@ -1,18 +1,31 @@
-from idc      import *
-from idaapi   import *
-from idautils import *
-
+"""
+  +---------------------------------------------------------------------------+
+  | IDA For Delphi                                                            |
+  | IDA Python Script to Get All function names from Event Constructor (VCL)  |
+  +---------------------------------------------------------------------------+
+      Copyright(c) 2022 - Coldzer0 <Coldzer0 [at] protonmail.ch> @Coldzer0x0
+"""
+from idc         import *
+from idaapi      import *
+from idautils    import *
+from ida_dbg     import *
+from ida_bytes   import *
+from ida_ida     import *
+from ida_idaapi  import *
+from ida_nalt    import *
+from ida_kernwin import *
+import collections
 
 addr = 0
 EP = 0
-info = idaapi.get_inf_structure()
+info = get_inf_structure()
 
 def get_ret(addr):
     out = addr
     count = 0
     while True:
-        # check for ret 
-        if Byte(out) == 0xc3:
+        # check for return 
+        if get_byte(out) == 0xc3:
             return out
         else:
             out += 1
@@ -31,57 +44,74 @@ class MyDbgHook(DBG_Hooks):
         global Monitor
         
         if (ea == addr):
-            RefreshDebuggerMemory()
+            ida_dbg.refresh_debugger_memory()
             func_name = ""
             func_addr = 0
             if (info.is_64bit()):
-                func_name = idc.GetString(GetRegValue("RDI") , int(GetRegValue("RDI")-4), strtype=idaapi.ASCSTR_UNICODE)
-                func_addr = GetRegValue("RAX")
+                func_name = ida_bytes.get_strlit_contents(idc.get_reg_value("RDX")+1 , get_byte(idc.get_reg_value("RDX")), ida_nalt.STRTYPE_C)
+                func_addr = idc.get_reg_value("RAX")
             elif (info.is_32bit()):
-                func_name = idc.GetString(GetRegValue("EDX")+1 , Byte(GetRegValue("EDX")), ASCSTR_C)
-                func_addr = GetRegValue("EAX")
+                func_name = ida_bytes.get_strlit_contents(idc.get_reg_value("EDX")+1 , get_byte(idc.get_reg_value("EDX")), ida_nalt.STRTYPE_C)
+                func_addr = idc.get_reg_value("EAX")
             else:
-                print 'Not Supported !'
+                print('Not Supported !')
+                return
             # add it to the BP list
-            idc.AddBpt(func_addr)
-            try:
-                idc.MakeCode(func_addr) # Mark as Code if not ..
-                idc.MakeFunction(func_addr) # Mark as Function if not ..
-                idc.MakeNameEx(func_addr, "_DE_"+func_name, idc.SN_NOWARN)
-            except:
-                pass
+            idc.add_bpt(func_addr)
             
-            print "func addr : 0x%x name : %s" % ( func_addr , func_name )
+            idc.create_insn(func_addr) # Mark as Code if not ..
+            funcPtr = ida_funcs.get_func(func_addr)
+            if not funcPtr:
+                ida_funcs.add_func(func_addr) # Mark as Function if not ..            
+            name = "_DE_{}".format(func_name.decode("utf-8")) # DE -> DelphiEvent
+            idc.set_name(func_addr, name, SN_NOWARN | SN_NOCHECK | SN_PUBLIC | SN_FORCE)
+            
+            print("func addr : 0x%x name : %s" % ( func_addr , name ))
+
             idaapi.continue_process()
         if (ea == EP):
             idaapi.continue_process()       
         return 0
     
     def dbg_process_exit(self, pid, tid, ea, code):
-        print("Process exited pid=%d tid=%d ea=0x%x code=%d" % (pid,
-            tid, ea, code))
+        print(("Process exited pid=%d tid=%d ea=0x%x code=%d" % (pid,
+            tid, ea, code)))
 
 
 
 # main
+zero_ea = 0
+pattern = ida_idaapi.BADADDR
+patterns = ida_bytes.compiled_binpat_vec_t()
+encoding = ida_nalt.get_default_encoding_idx(ida_nalt.BPU_1B)
 
 if (info.is_64bit()):
-    pattern = FindBinary(0, SEARCH_DOWN, "80 ?? ?? 00 74 ?? E8 ?? ?? ?? ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ?? ?? C3");
+    pattern_text = "80 ?? ?? 00 74 ?? E8 ?? ?? ?? ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ?? ?? C3"
+    err = ida_bytes.parse_binpat_str(patterns, zero_ea, pattern_text, 16, encoding)
+    if not err:
+        pattern = ida_bytes.bin_search(zero_ea, ida_ida.inf_get_max_ea(), patterns, ida_bytes.BIN_SEARCH_FORWARD | ida_bytes.BIN_SEARCH_NOBREAK | ida_bytes.BIN_SEARCH_NOSHOW)       
 elif (info.is_32bit()):
-    pattern = FindBinary(0, SEARCH_DOWN, "80 E3 DF 75 ?? 49 75 ?? 8B 46 02 ?? ?? 5B C3");
+    pattern_text = "80 E3 DF 75 ?? 49 75 ?? 8B 46 02 ?? ?? 5B C3"
+    err = ida_bytes.parse_binpat_str(patterns, zero_ea, pattern_text, 16, encoding)
+    if not err:
+        pattern = ida_bytes.bin_search(zero_ea, ida_ida.inf_get_max_ea(), patterns, ida_bytes.BIN_SEARCH_FORWARD | ida_bytes.BIN_SEARCH_NOBREAK | ida_bytes.BIN_SEARCH_NOSHOW)
 else:
-    print 'Not Supported !'
-    pattern = 0
+    print('Not Supported !')
+    exit()
 
-print "pattern addr : 0x%x" % pattern
+if pattern == ida_idaapi.BADADDR:
+    print("The Pattern not found")
+    exit()
+
+print("pattern addr : 0x%x" % pattern)
 addr = get_ret(pattern)
 
-print "Event Constructor addr : 0x%x" % addr
+print("Event Constructor addr : 0x%x" % addr)
 if addr > 0:
 
-    idc.AddBpt(addr);
-    if idc.CheckBpt(addr) >= 0:
-        print "BP add at %x" % addr
+    idc.add_bpt(addr);
+    if ida_dbg.check_bpt(addr) >= 0:
+        print("BP add at %x" % addr)
         # set the Dbg hook      
         try:
             if debughook:
@@ -95,15 +125,14 @@ if addr > 0:
         debughook.hook()
         print("Add new debughook ...")
         
-        EP = GetLongPrm(INF_START_IP)
-        idc.AddBpt(EP)
+        EP = idc.get_inf_attr(INF_START_IP)
+        ida_dbg.add_bpt(EP)
         
-        print "Entry point at 0x%x " % EP
+        print("Entry point at 0x%x " % EP)
          
-        request_run_to(EP)
+        idaapi.run_to(EP)
               
         # Start debugging
         run_requests()   
 else:
-    print "The Pattern not found"
-
+    print("The Pattern not found")
